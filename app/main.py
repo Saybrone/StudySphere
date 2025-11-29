4c63035953f7ec9d03c8e78b4e1e59f090596e26
Saybrone <batupekcan9@gmail.com>
2025-11-29 18:59:38 +0300

    Added MyNotes Feature

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
-from fastapi import FastAPI, Depends, HTTPException, Request, Form, Cookie
+from fastapi import FastAPI, Depends, HTTPException, Request, Form, Cookie, UploadFile, File
 from fastapi.responses import HTMLResponse,RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from .database import SessionLocal, Base, engine
-from .models import User
+from .models import User, Note
 from datetime import datetime, timedelta
 from jose import jwt
 from passlib.context import CryptContext
 import hashlib
 import os
 from jose import JWTError, jwt
+from fastapi.staticfiles import StaticFiles
+import os
+
+
 app = FastAPI()
 
 app.add_middleware(
@@ -22,7 +26,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
+os.makedirs("uploads", exist_ok=True)
+
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
@@ -32,21 +40,34 @@ pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 Base.metadata.create_all(bind=engine)
 
 
+def get_db(): 
+    db = SessionLocal() 
+    try: yield db 
+    finally: db.close()
+def get_current_user(
+    access_token: str = Cookie(None), 
+    db: Session = Depends(get_db)
+):
+    if not access_token:
+        raise HTTPException(status_code=401, detail="Not authenticated")
 
-def get_current_user(token: str):
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    return payload.get("sub")
+    try:
+        payload = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("sub")
+    except:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = db.query(User).filter(User.email == email).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
 
 def safe_password(password: str) -> str:
     return hashlib.sha256(password.encode("utf-8")).hexdigest()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+
 def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=60)
     to_encode = data.copy()
@@ -56,7 +77,7 @@ def create_access_token(data: dict):
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
-    return RedirectResponse("/dashboard")
+    return RedirectResponse("/login-page")
 
 @app.get("/login-page", response_class=HTMLResponse)
 def open_login_page(request: Request):
@@ -90,7 +111,7 @@ def signup(
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    return RedirectResponse("/login-page")
+    return RedirectResponse("/login-page", status_code=303)
 
 @app.post("/login")
 def login(
@@ -117,14 +138,74 @@ def logout():
     return response
 
 @app.get("/dashboard", response_class=HTMLResponse)
-def dashboard(request: Request, access_token: str = Cookie(None)):
-    if not access_token:
-        return RedirectResponse("/login-page")
+def dashboard(request: Request, user=Depends(get_current_user)):
+    return templates.TemplateResponse(
+        "Dashboard.html",
+        {"request": request, "email": user.email}
+    )
 
-    try:
-        user_email = get_current_user(access_token)
-    except:
-        return RedirectResponse("/login-page")
 
-    return templates.TemplateResponse("Dashboard.html", {"request": request, "email": user_email})
+@app.get("/my-notes", response_class=HTMLResponse)
+def my_notes_page(request: Request, user=Depends(get_current_user), db: Session = Depends(get_db)):
+    notes = db.query(Note).filter(Note.user_id == user.id).all()
+    return templates.TemplateResponse(
+        "my_notes.html",
+        {"request": request, "email": user.email, "notes": notes}
+    )
 
+
+@app.post("/notes/create")
+async def create_note(
+    title: str = Form(...),
+    content: str = Form(...),
+    file: UploadFile = File(None),   # <--- Add this
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    file_path = None
+
+    if file:
+        upload_dir = "uploads"
+        os.makedirs(upload_dir, exist_ok=True)
+
+        file_path = f"{upload_dir}/{user.id}_{file.filename}"
+
+        with open(file_path, "wb") as buffer:
+            buffer.write(await file.read())
+
+    note = Note(
+        title=title,
+        content=content,
+        user_id=user.id,
+        file_path=file_path
+    )
+
+    db.add(note)
+    db.commit()
+
+    return RedirectResponse("/my-notes", status_code=303)
+
+@app.post("/notes/delete/{note_id}")
+def delete_note(
+    note_id: int,
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    note = db.query(Note).filter(
+        Note.id == note_id,
+        Note.user_id == user.id
+    ).first()
+
+    if not note:
+        raise HTTPException(status_code=404, detail="Note not found")
+    if note.file_path:
+        print(note.file_path)
+        file_full_path = note.file_path
+        if os.path.exists(file_full_path):
+            os.remove(file_full_path)
+            print("Ok")
+        else:
+            print("not Ok")
+    db.delete(note)
+    db.commit()
+    return RedirectResponse("/my-notes", status_code=303)
--- app/models.py
+++ app/models.py
@@ -3,16 +3,6 @@ from sqlalchemy.orm import relationship
 from .database import Base
 from datetime import datetime
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-
-    notes = relationship("Note", back_populates="user", cascade="all, delete")
-
 
 class Note(Base):
     __tablename__ = "notes"
@@ -21,6 +11,18 @@ class Note(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     title = Column(String(255), nullable=False)
     content = Column(Text, nullable=False)
+    file_path = Column(String(255), nullable=True)
+
     created_at = Column(TIMESTAMP, default=datetime.utcnow)
     updated_at = Column(TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
     user = relationship("User", back_populates="notes")
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    notes = relationship("Note", back_populates="user", cascade="all, delete")
